bbc48310fe91288c645a4cc43856d9a61582dc14
gngeorgiev <gngeorgiev.it@gmail.com>
2016-03-17 14:12:15 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+navigator.serviceWorker.register('./worker.js')
+    .then(function (registration) {
+        return navigator.serviceWorker.ready.then(function (registration) {
+            return registration;
+        });
+    })
+    .then(function (registration) {
+        return registration.pushManager.getSubscription()
+            .then(function (subscription) {
+                if (subscription) {
+                    return subscription;
+                }
+                
+                return registration.pushManager.subscribe();
+            });
+    })
+    .then(function (subscription) {
+        fetch('/send?endpoint=' + subscription.endpoint, {
+            method: 'post'
+        })
+    })
---
+++ server.js
@@ -0,0 +1,24 @@
+var webPush = require('web-push');
+var express = require('express');
+var app = express();
+
+webPush.setGCMAPIKey('AIzaSyCNrzhY26lZC3eXaq96otA9GLllh1caOl0');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.use(express.static('.'))
+
+app.post('/send', (req, res) => {
+    webPush.sendNotification(req.query.endpoint, 15).then(function () {
+        res.sendStatus(201);   
+    }, function (err) {
+        console.log(err);
+        res.sendStatus(500)
+    });
+});
+
+app.listen(8026);
